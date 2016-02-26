1d3e6c14751b375a5bce3907d0a8dbc71afa0436
Hugh Giddens <hgiddens@atlassian.com>
2016-02-26 18:26:11 +1100

    Done up to "Fetching from the server".

--- build.sbt
+++ build.sbt
@@ -17,7 +17,8 @@ libraryDependencies ++= Seq(
 )
 jsDependencies ++= Seq(
   "org.webjars.bower" % "react" % "0.14.7" / "react-with-addons.js" minified "react-with-addons.min.js" commonJSName "React",
-  "org.webjars.bower" % "react" % "0.14.7" / "react-dom.js" minified  "react-dom.min.js" dependsOn "react-with-addons.js" commonJSName "ReactDOM"
+  "org.webjars.bower" % "react" % "0.14.7" / "react-dom.js" minified  "react-dom.min.js" dependsOn "react-with-addons.js" commonJSName "ReactDOM",
+  "org.webjars" % "marked" % "0.3.2-1" / "marked.js"
 )
 
 scalaJSUseRhino := false
--- src/main/scala/reacting/Tutorial.scala
+++ src/main/scala/reacting/Tutorial.scala
@@ -1,8 +1,84 @@
 package reacting
 
+import japgolly.scalajs.react.{PropsChildren, ReactNode, ReactComponentB, ReactDOM}
+import japgolly.scalajs.react.vdom.prefix_<^._
+import org.scalajs.dom.document
+
+import scala.scalajs.js
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSName
+
+@js.native
+sealed trait MarkedOptions extends js.Object {
+  val sanitize: Boolean = js.native
+}
+object MarkedOptions {
+  def apply(sanitize: Boolean): MarkedOptions =
+    js.Dynamic.literal(sanitize = sanitize).asInstanceOf[MarkedOptions]
+}
+
+@JSName("marked")
+@js.native
+object Marked extends js.Object {
+  def apply(x: String): String = js.native
+  def apply(x: String, options: MarkedOptions): String = js.native
+}
+
+object Model {
+  final case class Comment(id: Int, author: String, text: String)
+}
 
 object Tutorial extends JSApp {
+  val data = Seq(
+    Model.Comment(1, "Pete Hunt", "This is one comment"),
+    Model.Comment(2, "Jordan Walke", "This is *another* comment")
+  )
+
+  object Comment {
+    val component = ReactComponentB[Model.Comment]("Comment").
+      stateless.
+      noBackend.
+      render_P {
+        case Model.Comment(_, author, comment) =>
+          <.div(
+            ^.className := "comment",
+            <.h2(
+              ^.className := "commentAuthor", author
+            ),
+            <.span(^.dangerouslySetInnerHtml(Marked(comment, MarkedOptions(sanitize = true))))
+          )
+      }.build
+    def apply(comment: Model.Comment) =
+      component(comment)
+  }
+
+  val CommentList = ReactComponentB[Seq[Model.Comment]]("CommentList").
+    stateless.
+    noBackend.
+    render_P { data =>
+      val commentNodes = data.map(Comment(_))
+      <.div(^.className := "commentList", commentNodes)
+    }.build
+
+  val CommentForm = ReactComponentB[Unit]("CommentForm").
+    stateless.
+    noBackend.
+    render { $ =>
+      <.div(^.className := "commentForm", "Hello world! I am a CommentForm.")
+    }.buildU
+
+  val CommentBox = ReactComponentB[Seq[Model.Comment]]("CommentBox").
+    stateless.
+    noBackend.
+    render_P { data =>
+      <.div(
+        ^.className := "commentBox",
+        <.h1("Comments"),
+        CommentList(data),
+        CommentForm()
+      )
+    }.build
+
   def main() =
-    println("Hello, world!")
+    ReactDOM.render(CommentBox(data), document.getElementById("content"))
 }
